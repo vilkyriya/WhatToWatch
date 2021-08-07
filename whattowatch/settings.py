9b2fdc7176b3beb84a8f44f6c72aca253a37666e
vilkyriya <vilkyriya@gmail.com>
2021-08-08 01:17:05 +0700

    'Update'

--- whattowatch/settings.py
+++ whattowatch/settings.py
@@ -153,5 +153,3 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-FIXTURE_DIRS = os.path.join(BASE_DIR, 'catalog/fixtures/')
