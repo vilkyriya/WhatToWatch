6f9b30eca5f7b94dc987a84fe3f216c0428ea160
vilkyriya <vilkyriya@gmail.com>
2021-09-04 18:35:20 +0700

    IS-6 Вынесла группы отдельно в group.html. Сделала показ первых 5 из группы, остальные раскрываются по нажатию кнопки. Текущая композиция подсвечивается и не является активной ссылкой.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, CatalogPageView, CompositionView, change_watched_episodes
+from .views import *
 from django.contrib.auth.decorators import login_required
 
 
